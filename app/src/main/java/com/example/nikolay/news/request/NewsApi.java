975c394b3c3b1253eaf53f5134ace6317a9b01b1
NikCapko <nik.capko@mail.ru>
2018-12-09 18:54:54 +0300

    fix request

--- app/src/main/java/com/example/nikolay/news/request/NewsApi.java
+++ app/src/main/java/com/example/nikolay/news/request/NewsApi.java
@@ -2,13 +2,14 @@ package com.example.nikolay.news.request;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface NewsApi {
 
-    @GET("get-news")
-    Call<NewsModel> getNews(@Query("count") String count);
+    @GET("get-news/{count}")
+    Call<NewsModel> getNews(@Path("count") String count);
 
-    @GET("news-view")
-    Call<Void> newsView(@Query("id1") String newsId);
+    @GET("news-view/{id1}")
+    Call<Void> newsView(@Path("id1") String newsId);
 }
